15a1d7d51f543f7159305e97789eda37114b7f2b
Govind <govind12771277@gmail.com>
2026-06-14 04:13:07 +0530

    relative numbers and removed ~

--- lua/govind/set.lua
+++ lua/govind/set.lua
@@ -27,4 +27,10 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.fillchars = {
+  eob = " ",
+}
 -- vim.opt.colorcolumn = "150"
