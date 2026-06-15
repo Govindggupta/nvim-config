40b23e0c438f00b51de08ea0232892b1ed59348b
Govind <govind12771277@gmail.com>
2026-06-15 17:24:10 +0530

    added : git status aside the file changed

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,30 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            enable_git_status = true,
+
+            default_component_configs = {
+                git_status = {
+                    symbols = {
+                        added     = "A",
+                        modified  = "M",
+                        deleted   = "D",
+                        renamed   = "R",
+                        untracked = "U",
+                        ignored   = "I",
+                        unstaged  = "*",
+                        staged    = "✓",
+                        conflict  = "!",
+                    },
+                },
+            },
+        }  
     },
-    opts = {
-    },
-  },
 }
