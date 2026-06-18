7797efac876718c61e50bdf2d97302be4dc19922
Govind <govind12771277@gmail.com>
2026-06-18 14:16:07 +0530

    changed gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,18 +4,20 @@ return {
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
+      add          = { text = '┃' },
+      change       = { text = '┃' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
       changedelete = { text = '~' },
+      untracked    = { text = '┆' },
     },
     signs_staged = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
+      add          = { text = '┃' },
+      change       = { text = '┃' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
       changedelete = { text = '~' },
+      untracked    = { text = '┆' },
     },
   },
 }
